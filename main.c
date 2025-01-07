e1f555e90d6ce65d1850e3f9efb17ca04b801522
bartoszkorszun <bartoszkorszun@gmail.com>
2025-01-07 19:19:51 +0100

    asdf

--- main.c
+++ main.c
@@ -12,25 +12,6 @@
 #define TAG_REQUEST_ARBITER 3
 #define TAG_RELEASE_ARBITER 4
 
-void process_function(int rank, int num_processes);
-
-int main(int argc, char **argv) {
-    int rank, num_processes;
-
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
-
-    init_lamport_clock();
-    init_arbitration(NUM_ARBITERS);
-
-    process_function(rank, num_processes);
-
-    cleanup_arbitration();
-    MPI_Finalize();
-    return 0;
-}
-
 void process_function(int rank, int num_processes) {
     while (1) {
         // Zgłaszanie chęci uczestnictwa w zawodach
@@ -65,4 +46,49 @@ void process_function(int rank, int num_processes) {
         MPI_Barrier(MPI_COMM_WORLD); // Synchronizacja cykli
         sleep(rand() % 5 + 1); // Losowy czas oczekiwania
     }
+}
+
+void check_thread_support(int provided)
+{
+    printf("THREAD SUPPORT: chcemy %d. Co otrzymamy?\n", provided);
+    switch (provided) {
+        case MPI_THREAD_SINGLE: 
+            printf("Brak wsparcia dla wątków, kończę\n");
+            /* Nie ma co, trzeba wychodzić */
+	    fprintf(stderr, "Brak wystarczającego wsparcia dla wątków - wychodzę!\n");
+	    MPI_Finalize();
+	    exit(-1);
+	    break;
+        case MPI_THREAD_FUNNELED: 
+            printf("tylko te wątki, ktore wykonaly mpi_init_thread mogą wykonać wołania do biblioteki mpi\n");
+	    break;
+        case MPI_THREAD_SERIALIZED: 
+            /* Potrzebne zamki wokół wywołań biblioteki MPI */
+            printf("tylko jeden watek naraz może wykonać wołania do biblioteki MPI\n");
+	    break;
+        case MPI_THREAD_MULTIPLE: printf("Pełne wsparcie dla wątków\n"); /* tego chcemy. Wszystkie inne powodują problemy */
+	    break;
+        default: printf("Nikt nic nie wie\n");
+    }
+}
+
+int main(int argc, char **argv) {
+    int rank, num_processes;
+
+    int provided;
+    MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
+    check_thread_support(provided);
+    srand(rank);
+
+    MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    // init_lamport_clock();
+    // init_arbitration(NUM_ARBITERS);
+
+    // process_function(rank, num_processes);
+
+    // cleanup_arbitration();
+    MPI_Finalize();
+    return 0;
 }
