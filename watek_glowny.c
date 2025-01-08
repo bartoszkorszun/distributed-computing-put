61b46f62023686621091324928720cc8a4d6393b
bartoszkorszun <bartoszkorszun@gmail.com>
2025-01-08 21:09:46 +0100

    zmiana komentarzy i dodanie wcięć w kodzie dla czytelności

--- watek_glowny.c
+++ watek_glowny.c
@@ -10,49 +10,43 @@ void mainLoop()
     while (stan != InFinish) {
 	switch (stan) {
 	    case InRun: 
-		perc = random()%100;
-		if ( perc < 25 ) {
-		    println("Ubiegam się o sekcję krytyczną")
-		    packet_t *pkt = malloc(sizeof(packet_t));
-		    pkt->data = perc;
-		    ackCount = 0;
-		    for (int i=0;i<=size-1;i++)
-			if (i!=rank)
-			    sendPacket( pkt, i, REQUEST);
-		    changeState( InWant ); // w VI naciśnij ctrl-] na nazwie funkcji, ctrl+^ żeby wrócić
-					   // :w żeby zapisać, jeżeli narzeka że w pliku są zmiany
-					   // ewentualnie wciśnij ctrl+w ] (trzymasz ctrl i potem najpierw w, potem ]
-					   // między okienkami skaczesz ctrl+w i strzałki, albo ctrl+ww
-					   // okienko zamyka się :q
-					   // ZOB. regułę tags: w Makefile (naciśnij gf gdy kursor jest na nazwie pliku)
-		    free(pkt);
-		} // a skoro już jesteśmy przy komendach vi, najedź kursorem na } i wciśnij %  (niestety głupieje przy komentarzach :( )
-		break;
+			perc = random()%100;
+			if ( perc < 10 ) {
+				println("Chcę się napić")
+				packet_t *pkt = malloc(sizeof(packet_t));
+				pkt->data = perc;
+				ackCount = 0;
+				for (int i=0;i<=size-1;i++)
+					if (i!=rank)
+						sendPacket( pkt, i, REQUEST);
+				changeState( InWant ); 
+				free(pkt);
+			} // a skoro już jesteśmy przy komendach vi, najedź kursorem na } i wciśnij %  (niestety głupieje przy komentarzach :( )
+			break;
 	    case InWant:
-		println("Czekam na wejście do sekcji krytycznej")
-		// tutaj zapewne jakiś semafor albo zmienna warunkowa
-		// bo aktywne czekanie jest BUE
-		if ( ackCount == size - 1) 
-		    changeState(InSection);
-		break;
+			println("Czekam na utworzenie grupy")
+			// tutaj zapewne jakiś semafor albo zmienna warunkowa
+			// bo aktywne czekanie jest BUE
+			if ( ackCount > 2) 
+				changeState( InSection );
+			break;
 	    case InSection:
-		// tutaj zapewne jakiś muteks albo zmienna warunkowa
-		println("Jestem w sekcji krytycznej")
-		    sleep(5);
-		//if ( perc < 25 ) {
-		    println("Wychodzę z sekcji krytycznej")
-		    packet_t *pkt = malloc(sizeof(packet_t));
-		    pkt->data = perc;
-		    for (int i=0;i<=size-1;i++)
+			// tutaj zapewne jakiś muteks albo zmienna warunkowa
+			println("Jestem w grupie")
+				sleep(5);
+			println("Napojony kończę zawody i puszczam pawia lol")
+			packet_t *pkt = malloc(sizeof(packet_t));
+			pkt->data = perc;
+			for (int i=0;i<=size-1;i++)
 			if (i!=rank)
-			    sendPacket( pkt, (rank+1)%size, RELEASE);
-		    changeState( InRun );
-		    free(pkt);
-		//}
-		break;
+				sendPacket( pkt, (rank+1)%size, RELEASE);
+			changeState( InRun );
+			free(pkt);
+			break;
 	    default: 
-		break;
-            }
+			break;
+        }
+
         sleep(SEC_IN_STATE);
     }
 }
